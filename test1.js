69c656e6bd3a382161597455233e15dd23b046f2
Sagar Ghimire <sagar.ghimire@kotuko.it>
2021-06-15 12:24:53 +0545

    commented aayush console and added git command

--- test1.js
+++ test1.js
@@ -1,4 +1,8 @@
 console.log("I'm here")
 console.log("I'm a file")
 console.log('Hello world')
-console.log("I didn't write this text");
+// console.log("I didn't write this text");
+
+// git add .
+//git commit -m 'commented aayush console'
+//
